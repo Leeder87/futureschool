72c12389dab6b40a83790a53a27557285d11194f
Хармац Александр Борисович <hab@pscb.ru>
2020-01-17 23:09:04 +0300

    Added extra fields to Unit form

--- cms/admin.py
+++ cms/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django import forms
 
 # Register your models here.
 from .models import *
 
-
 admin.site.register(Course)
 admin.site.register(Theme)
 admin.site.register(Lesson)
-admin.site.register(Unit)
+
+
+class UnitForm(forms.ModelForm):
+    name = forms.CharField(max_length=200)
+    url = forms.CharField(max_length=200)
+
+    class Meta:
+        model = Unit
+        fields = ["name", "url", "unit_type"]
+
+
+class UnitAdmin(admin.ModelAdmin):
+    form = UnitForm
+
+
+admin.site.register(Unit, UnitAdmin)
